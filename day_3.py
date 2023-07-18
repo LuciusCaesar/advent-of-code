e1981f2bafcaf999869d3a0f467e14a6242d1f2f
Lucius Caesar <lucien.culot@gmail.com>
2023-07-18 13:42:41 +0000

    day_3 part 2

--- day_3.py
+++ day_3.py
@@ -1,11 +1,27 @@
 """Day 3 of the 2016 AoC"""
 
 instructions = []
+
+# For Part 1
 with open("day_3_input.txt", "r") as f:
     content = f.readlines()
     for line in content:
         instructions.append(line.strip().split())
-    
+
+
+# For Part 2
+new_instructions = []
+for r, row in enumerate(instructions):
+    triangle = [0,0,0]
+    for v, value in enumerate(row):
+        line = 3 * (r // 3) + v
+        column = r % 3
+        triangle[v] = instructions[line][column]
+    new_instructions.append(triangle)
+
+
+# only if part 2
+instructions = new_instructions
 
 def is_valid_sums(dimensions:list) -> bool:
     """Returns False triangular inequality is not respected in the set of three numbers"""
