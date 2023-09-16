343fd2592c430ad63e9668490d136be89ee4d71a
Lucius Caesar <lucien.culot@gmail.com>
2023-09-16 11:14:14 +0200

    Day 6 Part 1

---
+++ 2016/day_6.py
@@ -0,0 +1,14 @@
+"""Module to solve Day 6 of 2016"""
+from collections import Counter
+
+with open("2016/day_6_input.txt", "r") as file:
+    lines = [line.strip() for line in file.readlines()]
+
+message = ""
+for i in range(len(lines[0])):
+    column = [line[i] for line in lines]
+    
+    c = Counter(column).most_common()
+    message += c[0][0]
+
+print(message)
